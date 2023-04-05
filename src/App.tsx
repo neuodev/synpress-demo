3ae7d9d906dc63dfe897f0ff029d6f8b052f93a9
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-04-05 21:21:18 +0200

    feat: connect to MetaMask & display wallet address using wagmi

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import SynpressLogo from "/synpress.png";
+import CypressLogo from "/cypress.svg";
+import "./App.css";
+import {
+  WagmiConfig,
+  createClient,
+  configureChains,
+  useConnect,
+  useAccount,
+  useDisconnect,
+} from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { publicProvider } from "wagmi/providers/public";
+import { mainnet, optimism } from "wagmi/chains";
+
+const chains = [mainnet, optimism];
+const { provider } = configureChains(chains, [publicProvider()]);
+
+const metamaskConnector = new MetaMaskConnector({
+  chains,
+  options: {
+    shimDisconnect: true,
+  },
+});
+
+export const client = createClient({
+  autoConnect: true,
+  connectors: [metamaskConnector],
+  provider,
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { address, isConnected } = useAccount();
+  const { connect } = useConnect({
+    connector: metamaskConnector,
+  });
+  const { disconnect } = useDisconnect();
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://www.cypress.io/" target="_blank">
+          <img src={CypressLogo} className="logo" alt="Synpress logo" />
+        </a>
+        <a href="https://github.com/Synthetixio/synpress" target="_blank">
+          <img src={SynpressLogo} className="logo" alt="Synpress logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Synpress + Cypress + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        {isConnected ? (
+          <button onClick={() => disconnect()}>Disconnect</button>
+        ) : (
+          <button onClick={() => connect()}>Connect</button>
+        )}
+        <p id="address">Address: {address || "??"}</p>
+        <p id="connected">Connected: {isConnected ? "YES" : "NO"}</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>cypress/e2e/example.cy.ts</code> and save
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Synpress and Cypress logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App, { client } from "./App";
+import "./index.css";
+import { WagmiConfig } from "wagmi";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WagmiConfig client={client}>
+      <App />
+    </WagmiConfig>
+  </React.StrictMode>
+);
